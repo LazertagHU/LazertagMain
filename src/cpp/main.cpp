10d6198f9dc3156b600ee652a0ac624c098fb24a
Nathan Houwaart <nathan.houwaart@student.hu.nl>
2019-11-06 16:16:02 +0100

    added some comments

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -11,8 +11,6 @@ int main(){
     auto laserSight         = hwlib::target::pin_out( hwlib::target::pins::d1 );
     auto led                = hwlib::target::d2_36kHz();
     auto reload             = hwlib::target::pin_in( hwlib::target::pins::d4);
-
-    
     auto ir_sensor          = target::pin_in( target::pins::d8 );   
     auto tsop_gnd           = target::pin_out( target::pins::d9 );
     auto tsop_vdd           = target::pin_out( target::pins::d10 );
@@ -24,13 +22,11 @@ int main(){
     /* constructing player pool */
     auto player = playerInfo();
     auto hits = hit();
-    
-    
-
     auto playerpool         = rtos::pool<playerInfo>("playerpool");
 
+
+    /* constructing tasks */
     auto display            = displayTaak(8, "DisplayTaak");
-    //auto transfer           = TransferHitsControlTaak(9, "TransferHitsControlTaak",playerpool);
     auto transmitter        = sendTask(1, "sendTask", led, laserSight, 1000 );
     auto speaker            = speakerTaak(4, "SpeakerTaak");
     auto runGame            = runGameTaak(6, "RunGameTaak",display, transmitter, playerpool, speaker);   
